c58df3b020621a168cfba39651341b858a0e6176
Awake888 <165668565+Awake888@users.noreply.github.com>
2025-07-11 09:53:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -169,11 +169,11 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // Убедитесь, что пути к изображениям правильные и файлы существуют
     const photos = [
-        '/ipg/photo1.jpg',
-        '/ipg/photo2.jpg',
-        '/ipg/photo3.jpg',
-        '/ipg/photo4.jpg',
-        '/ipg/photo5.jpg'
+        'img/photo1.jpg',
+        'img/photo2.jpg',
+        'img/photo3.jpg',
+        'img/photo4.jpg',
+        'img/photo5.jpg',
     ];
     
     let currentSlide = 0;
