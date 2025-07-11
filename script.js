50a91c98f2aed52b5a13a55c71f6c027bce28f0f
Awake888 <165668565+Awake888@users.noreply.github.com>
2025-07-11 09:05:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -169,11 +169,11 @@ document.addEventListener('DOMContentLoaded', function() {
     
     // Замените эти пути на свои фотографии
     const photos = [
-    'img/photo1.jpg',
-    'img/photo2.jpg',
-    'img/photo3.jpg',
-    'img/photo4.jpg',
-    'img/photo5.jpg'
+    './img/photo1.jpg',
+    './img/photo2.jpg',
+    './img/photo3.jpg',
+    './img/photo4.jpg',
+    './img/photo5.jpg'
 ];
     
     let currentSlide = 0;
